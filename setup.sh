432271f48895990a43c6fc0e175c1746cbd7a9b9
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-04-20 13:42:33 +0900

    仮想マシンにファイルを転送

--- setup.sh
+++ setup.sh
@@ -6,6 +6,16 @@ boxName='centos7-develop'
 # Account Name of Vagrant Cloud.
 vcUser='takahiro-itou'
 
+# 所定のディレクトリを、仮想マシンへ転送する用意をする
+project_base_dir=$(readlink -f "${script_dir}/..")
+
+env     \
+    COPY_CONFIG_DIR='no'                        \
+    PROJECT_BASE_DIR="${project_base_dir}"      \
+    WORKDIR='/cygdrive/w/Vagrant'               \
+    /bin/bash -xue "${vagrant_dir}/common/setup-userdata.sh"    \
+    || exit $?
+
 pushd  vagrant
 
 vagrant  destroy -f  || exit $?

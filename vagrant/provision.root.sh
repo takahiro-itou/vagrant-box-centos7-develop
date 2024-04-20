05d7149e998a926cc60bf12f871219eeac193e62
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-04-20 13:26:30 +0900

    インストールパッケージを追加
    (1) screen  をインストール

--- vagrant/provision.root.sh
+++ vagrant/provision.root.sh
@@ -7,4 +7,7 @@ sudo  timedatectl  set-timezone Asia/Tokyo
 sudo  yum  update -y
 sudo  yum  groupinstall -y  "Base"  "Development Tools"
 sudo  yum  install -y  cmake  emacs  git  mercurial  \
-    clang  doxygen
+    clang  doxygen  screen
+
+sudo  mkdir  -p    /tools
+sudo  chmod  1777  /tools
